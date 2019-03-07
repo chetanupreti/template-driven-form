a3f2022145475f77742d8fc8a4c9a13a4dc08078
chetan <sidharthaupreti@.com>
2019-03-07 15:28:11 +0530

    1st

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,91 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularforms';
+   @ViewChild('f') signupForm:NgForm;
+    defaultValue="pet"
+    answer=''
+    genders=['male','female','others']
+    user={
+      userName:'',
+      email:'',
+      secretQuestion:'',
+      answer:'',
+      gender:''
+    }
+    submitted=false
+//  onSubmit(form:NgForm){
+//    console.log("submit",form)
+//  }
+    suggestUserName(){
+      const suggestedName='chetan upreti';
+      /*
+            in setValue we have to provide all the values for form and it override the form values.
+      */
+      // this.signupForm.setValue({
+      //   userData:{
+      //     userName:suggestedName,
+      //     email:''
+      //   },
+      //   userSecret:{
+      //     secret:'pet',
+      //     answer:''
+      //   },
+      //   gender:this.genders[0]
+      // })
+      // this.signupForm.setValue({
+      //   userData:{
+      //     userName:suggestedName,
+      //   },
+      //   userSecret:{
+      //     secret:'pet'
+      //   }
+      // })  
+       /*
+       in case of pathchValue no need of provide all values of form and it doesn't override the value of form 
+       */   
+      this.signupForm.form.patchValue({
+         userData:{
+          userName:suggestedName,
+        }
+      })
+    }
+
+   onSubmit(){
+    // console.log("submit",this.signupForm);
+    this.submitted=true;
+    this.user.userName=this.signupForm.value.userData.userName;
+    this.user.email=this.signupForm.value.userData.userName;
+    this.user.secretQuestion=this.signupForm.value.userSecret.secret;
+    this.user.answer=this.signupForm.value.userSecret.answer;
+    this.user.gender=this.signupForm.value.gender;
+   // this.signupForm.reset()  //for reset the form
+    // this.signupForm.form.patchValue({
+    //   userData:{
+    //     userName:'',
+    //     email:''
+    //   },
+    //   userSecret:{
+    //     secret:'pet',
+    //     answer:''
+    //   },
+    //   gender:this.genders[0]
+    // })
+   }
+   reset(){
+  //    this.submitted=false;
+  //    this.signupForm.form.patchValue({
+  //      userData:{
+  //        userName:'',
+  //        email:''
+  //      },
+  //    gender:this.genders[1],
+  //    userSecret:{
+  //      secret:"pet",
+  //      answer:''
+  //    }
+  //  })
+     
+   this.signupForm.reset();
+   
+   }
 }
